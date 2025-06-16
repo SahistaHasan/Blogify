71c55fc549a10e4eed3f9b80536d2ba5b1fbfd18
Sahista Hasan <sahistahasan001@gmail.com>
2025-06-16 18:33:45 +0530

    Added update blog post, show all blost, etc..features.

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,7 +1,17 @@
 import e from "express";
 import { adminLogin } from "../controllers/adminController.js";
+import { addBlog, deleteBlogById, getAllBlogs, getBlogById, publishToggle } from "../controllers/blogController.js";
+import upload from "../middleware/multer.js";
 
 const adminRouter = e.Router();
 
 adminRouter.post("/login",adminLogin);
+
+
+adminRouter.post('/add',upload.single('image'),addBlog);
+adminRouter.get('/all',getAllBlogs)
+adminRouter.get('/:blogId',getBlogById)
+adminRouter.delete('/delete',deleteBlogById)
+adminRouter.post('/publish-toggle',publishToggle)
+
 export default adminRouter;
